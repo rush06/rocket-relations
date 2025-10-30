a70c098aad8bc31b2c39c45f9e615c94b82677c0
rush06 <66142764+rush06@users.noreply.github.com>
2025-10-30 11:20:34 -0700

    test for ideal.py

---
+++ tests/test_ideal.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from rocket_relations.ideal import find_characteristic_velocity
+from rocket_relations.ideal import find_thrust_coefficient
+
+
+def test_char_vel():
+    assert find_characteristic_velocity(1.2, 350, 3500) == 1706.6214
+
+def test_thrust_coeff():
+    assert find_thrust_coefficient(0.02, 0.0125, 10, 1.2) == 1.5423079
